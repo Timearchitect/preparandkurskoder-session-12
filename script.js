cbe82732ed0baac41b62e4a84c44fb501ef7c39b
Alrik He <alrik_he@hotmail.com>
2024-12-16 18:26:36 +0100

    first

---
+++ script.js
@@ -0,0 +1,48 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+
+
+/* 
+
+    Av alrik he 
+    2024-12-13
+
+*/
+
+// 1) varabler
+let wallet    //skapar en variabel som heter wallet
+console.log(wallet)
+
+wallet = 100  // lägger vi in  nummer 100 i variabeln
+console.log(wallet)
+
+wallet = 200
+console.log(wallet)
+
+let foo //deklarerar en variabel som heter foo
+foo = "hejsan" // tilldelar variabel foo med en string "hejsan"
+foo = 88
+
+wallet = foo 
+
+wallet = foo + 1
+
+wallet =  (3 + 2) * 2  //kalkylation/calculation
+
+//P arentess
+//E xponent
+//M utliplikation
+//D ivition
+//A dddition
+//S ubtraktion
+
+console.log(wallet);
+
+let text = "hej"    //deklaration + tilldelning
+
+text = "hej" + "san"  //konkatination /concatination
+text = "hej" + (1 + 1)     //konkatination
+//text =  1 + 1  + "hej"    //konkatination
