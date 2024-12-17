742ab3169f02432b249d63049dcd5a67099999cb
Alrik He <alrik_he@hotmail.com>
2024-12-17 15:01:35 +0100

    t2

---
+++ tillfälle 2 2024-12-16/script.js
@@ -0,0 +1,63 @@
+//2) datatypes
+
+let x = false;
+// number
+x = 333; // int/integer,long :heltal
+x = -10.0005; // float/double : decimaltal
+
+//string
+x = "#"; // char/character : karaktär
+x = "hello world!!! "; // str/string : sträng
+
+//boolean
+x = true; // boolean : boolean
+
+//undefined
+x = undefined;
+
+//Object
+
+//interactions
+
+//string + string = string   //konkatination
+// "hej" + "san" = "hejsan"
+
+//string + number = string   //konkatination
+// "hej" +  1    = hej1
+
+//string + boolean = string   //konkatination
+// "hej" + true  = "hejtrue"
+
+// number + number = number   //kalkylation
+//    1   +    1   =  2
+
+// number + boolean = number  //kalkylation
+//   1    +  true   =  2
+
+// boolean + boolean = number //kalkylation
+// true    +  true   =  2
+
+// 3) if satser / vilkors satseer
+
+//condition (true/false)
+// 2 > 1 större än
+// 1 < 2 mindre än
+// 2 >= 1 större än eller likamed
+// 1 <= 2 mindre än eller likamed
+// 1 + 1 == 2  // jämnför
+// "hej" == "HEJ"
+// 1 == "1"  ->   true  //value
+// 1 == 1.0   -> true
+// 1 == true  -> true
+// 1 === 1  -> true //value + datatype
+// 1 != 2  -> true
+
+let mode = 3;
+
+if (mode === 3) {
+  console.log("YESS"); //true codes
+} else {
+  console.log("NOOO"); //false codes, optional
+}
+
+console.log("vidare...");
